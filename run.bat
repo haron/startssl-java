2bcf889201ea6653d4b30d9e498b55c115d8f5e2
Marius Gedminas <marius@gedmin.as>
2013-12-05 10:22:19 +0200

    Fix keystore path to match modern Java installations
    I spent a week headbanging against the wall trying to get my Jenkins slave to accept my StartSSL certificate.  Until I finally noticed that the Oracle Java installer created a directory structure that looks like this:
    ```
    c:\Program Files (x86)\Java\jre7\bin\keytool.exe
    c:\Program Files (x86)\Java\jre7\lib\security\cacerts
    ```
    
    If I set %JAVA_HOME% to the `...\Java\jre7` directory, run.bat fails to locate the keystore and fails to import the certificates.
    
    It didn't help that keytool prints "Certificate imported" success messages briefly in the command prompt, while the file not found errors are hidden away in run.log.

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
 cd %~dp0
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca -file ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class1 -file sub.class1.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class2 -file sub.class2.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class3 -file sub.class3.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class4 -file sub.class4.server.ca.crt >> run.log
+set KEYTOOL=%JAVA_HOME%\bin\keytool
+set KEYSTORE=%JAVA_HOME%\lib\security\cacerts
+set PASSWORD=changeit
+echo "Importing StartSSL certificates into %KEYSTORE%" >> run.log
+"%KEYTOOL%" -import -trustcacerts -keystore "%KEYSTORE%" -storepass %PASSWORD% -noprompt -alias startcom.ca -file ca.crt >> run.log
+"%KEYTOOL%" -import -trustcacerts -keystore "%KEYSTORE%" -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class1 -file sub.class1.server.ca.crt >> run.log
+"%KEYTOOL%" -import -trustcacerts -keystore "%KEYSTORE%" -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class2 -file sub.class2.server.ca.crt >> run.log
+"%KEYTOOL%" -import -trustcacerts -keystore "%KEYSTORE%" -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class3 -file sub.class3.server.ca.crt >> run.log
+"%KEYTOOL%" -import -trustcacerts -keystore "%KEYSTORE%" -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class4 -file sub.class4.server.ca.crt >> run.log
