bc9ae7b0ea1074c16b3b9a444d146c7c4b00fa37
Ilya Kargapolov <d3vil.st@gmail.com>
2014-10-20 17:42:25 +0400

    Fix Paths

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 KEYTOOL=keytool
-#KEYSTORE=%JAVA_HOME%\lib\security\cacerts
+KEYSTORE="$JAVA_HOME/jre/lib/security/cacerts"
 PASSWORD=changeit
-echo "Importing StartSSL certificates into " >> run.log
-$KEYTOOL -import -trustcacerts -storepass %PASSWORD% -noprompt -alias startcom.ca -file ca.crt >> run.log
-$KEYTOOL -import -trustcacerts -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class1 -file sub.class1.server.ca.crt >> run.log
-$KEYTOOL -import -trustcacerts -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class2 -file sub.class2.server.ca.crt >> run.log
-$KEYTOOL -import -trustcacerts -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class3 -file sub.class3.server.ca.crt >> run.log
-$KEYTOOL -import -trustcacerts -storepass %PASSWORD% -noprompt -alias startcom.ca.sub.class4 -file sub.class4.server.ca.crt >> run.log
+echo "Importing StartSSL certificates into $KEYSTORE" >> run.log
+$KEYTOOL -import -trustcacerts -keystore $KEYSTORE -storepass $PASSWORD -noprompt -alias startcom.ca -file ca.crt >> run.log
+$KEYTOOL -import -trustcacerts -keystore $KEYSTORE -storepass $PASSWORD -noprompt -alias startcom.ca.sub.class1 -file sub.class1.server.ca.crt >> run.log
+$KEYTOOL -import -trustcacerts -keystore $KEYSTORE -storepass $PASSWORD -noprompt -alias startcom.ca.sub.class2 -file sub.class2.server.ca.crt >> run.log
+$KEYTOOL -import -trustcacerts -keystore $KEYSTORE -storepass $PASSWORD -noprompt -alias startcom.ca.sub.class3 -file sub.class3.server.ca.crt >> run.log
+$KEYTOOL -import -trustcacerts -keystore $KEYSTORE -storepass $PASSWORD -noprompt -alias startcom.ca.sub.class4 -file sub.class4.server.ca.crt >> run.log
